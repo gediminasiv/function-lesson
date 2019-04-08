3a1201297c1d835a14033fc0cea913dd554c7581
gediminasiv <gediminasiv>
2019-04-08 19:30:37 +0300

    Add refactored game

---
+++ database.py
@@ -0,0 +1,13 @@
+# database.py
+
+def readFile(filename):
+    file = open(filename, "r")
+
+    return file.read()
+
+def writeFile(filename, data):
+    file = open(filename, "w")
+
+    file.write(str(data))
+
+    return readFile(filename)
